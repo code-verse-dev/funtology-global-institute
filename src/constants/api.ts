4de441b3256bcaa1675b19ec93e9f34ea2b27789
Umair6590 <umair.muhammad@salsoft.net>
2026-04-01 15:42:12 +0500

    Update live server URL in api.ts to point to the custom development environment for testing purposes.

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,7 +3,7 @@ const { hostname } = window.location;
 const servers = {
   local: "http://localhost:3031",
   customDev: "https://react.customdev.solutions:3031",
-  live: "https://funtologyglobalinstitute.com/",
+  live: "https://react.customdev.solutions:3031",
 };
 
 let publicUrl = "/";
